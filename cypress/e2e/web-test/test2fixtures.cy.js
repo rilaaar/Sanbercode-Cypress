088827ab1d6cc8c96a6df400bebed6dd729d19f7
Rila Afhrila <rilaafhrila15@gmail.com>
2025-01-20 11:31:12 +0700

    fixing error code testfixtures

--- cypress/e2e/web-test/test2fixtures.cy.js
+++ cypress/e2e/web-test/test2fixtures.cy.js
@@ -1,19 +1,32 @@
 describe('Login using fixture', () => {
-    beforeEach(() => {
-        cy.visit('')
-      })
+  beforeEach(() => {
+      cy.visit('')
+    })
 
-    it('Success Login', () => {
-      cy.fixture('usersSaucedemo.json').then((usersSaucedemo) => {
-        const datauser = usersSaucedemo[0];
-        cy.loginSaucedemo(datauser.username, datauser.password)
+  it('Success Login', () => {
+    cy.fixture('usersSaucedemo.json').then((usersSaucedemo) => {
+      const datauser = usersSaucedemo[0];
+      cy.loginSaucedemo(datauser.username, datauser.password)
+      });
+  })
+
+  it('Success Login (fixtures)', () => {
+    cy.fixture('usersSaucedemo.json').then((usersSaucedemo) => {
+      if (usersSaucedemo.length === 0) {
+         throw new Error('usersSaucedemo.json is empty.');
+      }
+       const datauser0 = usersSaucedemo[0];
+       cy.loginSaucedemo(datauser0.username, datauser0.password);
     });
+  })
 
-    // it('Success Login using data1', () => {
-    //     cy.fixture('usersSaucedemo.json').then((usersSaucedemo) => {
-    //       const datauser1 = usersSaucedemo[1];
-    //       cy.loginSaucedemo(datauser1.username, datauser1.password)
-    //   });
-
-});
+it('Success Login (fixtures)', () => {
+  cy.fixture('usersSaucedemo.json').then((usersSaucedemo) => {
+    if (usersSaucedemo.length === 0) {
+       throw new Error('usersSaucedemo.json is empty.');
+    }
+     const datauser1 = usersSaucedemo[1];
+     cy.loginSaucedemo(datauser1.username, datauser1.password);
+  });
+})
 })
